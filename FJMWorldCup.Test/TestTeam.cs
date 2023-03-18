541d4e8ea59ef7d0be84ec291634baa909189633
FJose-Medina <franmedinadev@gmail.com>
2023-03-18 13:03:19 +0100

    ADD Class Team and TestTeam

---
+++ FJMWorldCup.Test/TestTeam.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+
+namespace FJMWorldCup.Test;
+
+public class TestTeam
+{
+    [Fact]
+    public void TestConstructor()
+    {
+        // Arrange
+        string expectedName = "Team";
+        int expectedGoalsScored = 0;
+        
+        // Act
+        var team = new Team(expectedName);
+        
+        // Assert
+        Assert.Equal("Team", team.Name);
+        Assert.Equal(expectedGoalsScored, team.GoalsScored);
+    }
+    [Fact]
+    public void TestGoals()
+    {
+        // Arrange
+        int expectedGoals= 3;
+        Team team = new Team("Team");
+
+        // Act
+        team.GoalsScored = expectedGoals;
+
+        // Assert
+        Assert.Equal(expectedGoals, team.GoalsScored);
+    }
+}
---
+++ FJMWorldCup/Team.cs
@@ -0,0 +1,24 @@
+﻿namespace FJMWorldCup;
+
+public class Team
+{
+    private string _name;
+    private int _goalsScored;
+
+    public Team(string name)
+    {
+        _name = name;
+        _goalsScored = 0;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public int GoalsScored
+    {
+        get { return _goalsScored; }
+        set => _goalsScored = value;
+    }
+}
